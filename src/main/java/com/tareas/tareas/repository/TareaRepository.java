662789f048fbe096f0e9f744d6e726c73f188713
Xavi Alessandro Sanchez Villamizar <xavi.twt2005@gmail.com>
2026-05-12 13:56:22 -0500

    Initial commit

---
+++ src/main/java/com/tareas/tareas/repository/TareaRepository.java
@@ -0,0 +1,12 @@
+package com.tareas.tareas.repository;
+
+import com.tareas.tareas.entity.Tarea;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TareaRepository extends JpaRepository<Tarea, Long> {
+
+    List<Tarea> findByCompletada(boolean completada);
+
+}
